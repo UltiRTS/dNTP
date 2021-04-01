80871901ab900c5bf5ca3c324fe7c39bcdad13ce
bianyuanop <m13320507359@163.com>
2021-04-02 07:11:50 +0800

    seperate the downloader from SpringCrapper

--- main.py
+++ main.py
@@ -1,12 +1,97 @@
 import os
+import requests
+import threading
 from zkscrapper import zkScrap
 from springCrapper import SpringCrapper
+from difflib import SequenceMatcher
 
-if not os.path.exists('maps'):
-    os.makedirs('maps')
 
-zk=zkScrap()
-zk.start()
+class Downloader(threading.Thread):
+    '''
+    To plugin another downloader for map filter
+    the other downloader/mapListGetter must have these attr
+    - mapNames
+    - targetUrl
+    - _extractAllMapNames
+    '''
 
-sc = SpringCrapper()
-sc.start()
+    def __init__(self, downloaders=[], ratio=0.7, retry=3):
+        threading.Thread.__init__(Downloader)
+
+        self.downloaders = downloaders
+        self.targets = []
+        self.mapNames = []
+        self.compareRatio = ratio
+        self.retry = retry
+        self.defaultRetry = retry
+    
+    def _getAllLatestMap(self):
+        # sort all mapNames first
+        self.mapNames.sort(key=lambda x: x.get('mapName'))
+
+        mapNameIndex = 0
+        mapNamesLen = len(self.mapNames)
+
+        while mapNameIndex+1 < mapNamesLen:
+            curMapName = self.mapNames[mapNameIndex]['mapName']
+            nextMapName = self.mapNames[mapNameIndex+1]['mapName']
+
+            if SequenceMatcher(None, curMapName, nextMapName).ratio() > self.compareRatio:
+                self.mapNames.pop(mapNameIndex)
+                mapNamesLen -= 1
+            else:
+                mapNameIndex += 1
+            
+        return mapNamesLen
+
+    def run(self):
+        targetId = 0
+        for downloder in self.downloaders:
+            print("Parsing target url: ", downloder.targetUrl)
+            downloder._extractAllMapNames()
+            self.targets.append(downloder.targetUrl)
+            for mapName in downloder.mapNames:
+                self.mapNames.append({
+                    "targetUrlId": targetId,
+                    "mapName": mapName
+                })
+            
+            targetId += 1
+
+        print("Parsing finished. All map count: ", len(self.mapNames))
+
+        # vague filter
+        mapCount = self._getAllLatestMap()
+        print("Got all latest maps. map count: ", mapCount)
+
+        # start downloading 
+        mapIndex = 0
+        while mapIndex < mapCount:
+            mapName = self.mapNames[mapIndex]
+            try:
+                url = self.targets[mapName['targetUrlId']] + mapName['mapName']
+                resp = requests.get(url)
+                with open('maps/' + mapName['mapName'], 'wb') as f:
+                    f.write(resp.content)
+                
+                print("Downloaded {0}".format(mapName['mapName']))
+                mapIndex += 1
+            except Exception as e:
+                if self.retry < 0:
+                    self.retry = self.defaultRetry
+                    mapIndex += 1 
+                else: 
+                    print("Download failed {0} retrying {1} times".format(e, self.retry))
+                    self.retry -= 1
+
+
+if __name__ == '__main__':
+
+    if not os.path.exists('maps'):
+        os.makedirs('maps')
+
+    zk=zkScrap()
+    sc = SpringCrapper()
+
+    dl = Downloader(downloaders=[sc,])
+    dl.start()
--- springCrapper.py
+++ springCrapper.py
@@ -10,58 +10,58 @@ class SpringCrapper(threading.Thread):
 
     def __init__(self, ratio=0.7, retry=3):
         threading.Thread.__init__(SpringCrapper)
-        self.urls = []
+        self.mapNames = []
         self.targetUrl = "https://api.springfiles.com/files/maps/"
         self.compareRatio = ratio
         self.retry = 3
 
-    def _extractAllMapUrls(self):
+    def _extractAllMapNames(self):
         resp = requests.get(self.targetUrl)
         soup = BeautifulSoup(resp.text, 'html.parser')
         allATags = soup.select('a')
         for tag in allATags:
             tagHref = tag['href']
             if tagHref.endswith('sd7') or tagHref.endswith('sdz'):
-                self.urls.append(tagHref)
+                self.mapNames.append(tagHref)
         
-        return len(self.urls)
+        return len(self.mapNames)
     
     def _getAllLatestMap(self):
         index = 0
-        urlLenth = len(self.urls)
+        urlLenth = len(self.mapNames)
         while index+1 < urlLenth:
-            curMapName = self.urls[index].split('_')[0]
-            nextMapName = self.urls[index+1].split('_')[0]
+            curMapName = self.mapNames[index].split('_')[0]
+            nextMapName = self.mapNames[index+1].split('_')[0]
             # if next map name is very similiar to current one delete current one
             # cuz the origin map list is ordered by version, SO the logic here is just fine
             if SequenceMatcher(None, curMapName, nextMapName).ratio() > self.compareRatio:
-                self.urls.pop(index)
+                self.mapNames.pop(index)
                 urlLenth -= 1
             else: 
                 index += 1
         
-        #print(self.urls)
-        return len(self.urls)
+        #print(self.mapNames)
+        return len(self.mapNames)
 
     def run(self):
-        total = self._extractAllMapUrls()
+        total = self._extractAllMapNames()
         filtered = self._getAllLatestMap()
         print("Total map is {0}, aftering filter we have {1}".format(total, filtered))
-        urlLen = len(self.urls)
+        urlLen = len(self.mapNames)
         i = 0
         while i < urlLen:
             try:
-                resp = requests.get(self.targetUrl + self.urls[i])
-                with open('maps/' + self.urls[i], 'wb') as f:
+                resp = requests.get(self.targetUrl + self.mapNames[i])
+                with open('maps/' + self.mapNames[i], 'wb') as f:
                     f.write(resp.content)
                 
-                print("downloaded {0}".format(self.urls[i]))
+                print("downloaded {0}".format(self.mapNames[i]))
                 i += 1
             except Exception as e:
                 if self.retry < 0:
                     # reset retry
                     self.retry = 3
                     i += 1
-                print("{0} failed as error {1}".format(self.urls[i], e))
+                print("{0} failed as error {1}".format(self.mapNames[i], e))
                 self.retry -= 1
                 print("retrying left {0}".format(self.retry))
