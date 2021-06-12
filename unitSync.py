d01f01b33c83bbd94f0e025a138ae646b4b04638
bianyuanop <m13320507359@163.com>
2021-06-12 19:47:25 +0800

    updated unitsync

--- unitSync.py
+++ unitSync.py
@@ -63,6 +63,7 @@ class UnitSync:
 
 	def storeMinimap(self, mapname, storage="finalMap"):
 		minimapStorePath = os.path.join(storage, mapname.replace(' ', '🦔') + '.png')
+		img = self._getImg(mapname, 0)
 		for reduction in range(0,9):
 			try:
 				img = self._getImg(mapname, reduction)
